597360d9562c2d4bbb5d1f6e14e87791c8a15d59
paolaguerra <paolaguerraph@gmail.com>
2023-04-25 12:47:49 -0400

    Simular eventos - Click

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Pruebas en el <CounterApp />', () => { 
@@ -17,4 +17,32 @@ describe('Pruebas en el <CounterApp />', () => {
 
       });
 
- });
+      test('debe de incrementar con el boton +1', () => { 
+
+        render( <CounterApp value={10} /> );
+        fireEvent.click( screen.getByText('+1') );
+        expect( screen.getByText('11') ).toBeTruthy();
+
+       });
+
+       test('debe de decrementar con el boton -1', () => { 
+
+        render( <CounterApp value={10} /> );
+        fireEvent.click( screen.getByText('-1') );
+        expect( screen.getByText('9') ).toBeTruthy();
+
+       });
+
+       test('debe de funcionar el boton de reset', () => { 
+
+        render( <CounterApp value={10} /> );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        // fireEvent.click( screen.getByText('Reset') );
+        fireEvent.click(screen.getByRole('button', {name: "btn-reset"}));
+        expect( screen.getByText('10') ).toBeTruthy();
+
+       });
+
+ }); 
